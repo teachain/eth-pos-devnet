df92e37d67dc1f7ae2b528e997e5790d58855834
teachain <281477138@qq.com>
2025-05-08 16:21:09 +0800

    for prysm v6.0.1 and geth v 1.15.11

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
-docker rm -f $(docker ps -a -q)
+#!/bin/bash
+
+docker-compose -f docker-compose.yml down
 rm -Rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
 rm -Rf ./execution/geth
